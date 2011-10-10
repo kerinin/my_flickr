34bd8b90b714990a0380779b4d35eac822cf8769
Ryan Michael <kerinin@gmail.com>
2011-10-10 16:53:14 -0500

    initial import

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Flickr::Application.config.secret_token = 'b8b84a03f7f762eaeb11d86fc5b4d67a6ebfdc306a300c185533293991976abf51d681a3ed73a3448fd010cbe7890da6fdf34222d46bf37276ce6ce0f133ba87'
